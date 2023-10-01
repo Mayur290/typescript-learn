22b48b60c0fde0ac5038a8a65bec8788c6f45bb3
mayur bansal <mayurbansal@mayurs-MacBook-Air.local>
2023-10-01 22:24:10 +0530

    base setup

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,7 @@
-let a: number = 2;
-let b: number = 6;
-let c: number = 2;
-
+"use strict";
+let a = 2;
+let b = 6;
+let c = 2;
 console.log(a / b);
 // in js(not ts) coerced the data into type what it wanted
-
 // ts infers what type of data
