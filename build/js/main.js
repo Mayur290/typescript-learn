4795df5c1ec3412e16dab1725de0540f8798ecf7
mayur bansal <mayurbansal@mayurs-MacBook-Air.local>
2023-10-01 22:33:36 +0530

    ts types

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,19 @@
 "use strict";
-let a = 2;
-let b = 6;
-let c = 2;
-console.log(a / b);
-// in js(not ts) coerced the data into type what it wanted
-// ts infers what type of data
+// let a: number = 2;
+// let b: number = 6;
+// let c: number = 2;
+// console.log(a / b);
+// // in js(not ts) coerced the data into type what it wanted
+// // ts infers what type of data
+// let mname: string = "mayur";
+// let meaningOfLife: number;
+// let isLoading: boolean;
+// let album: any; //impotant
+// mname = "mayur bansal";
+// meaningOfLife = 22;
+// isLoading = true;
+// album = 1998;
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// };
+// let postId: string | number;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-let a: number = 2;
-let b: number = 6;
-let c: number = 2;
+// let a: number = 2;
+// let b: number = 6;
+// let c: number = 2;
 
-console.log(a / b);
-// in js(not ts) coerced the data into type what it wanted
+// console.log(a / b);
+// // in js(not ts) coerced the data into type what it wanted
 
-// ts infers what type of data
+// // ts infers what type of data
+
+// let mname: string = "mayur";
+// let meaningOfLife: number;
+// let isLoading: boolean;
+
+// let album: any; //impotant
+
+// mname = "mayur bansal";
+// meaningOfLife = 22;
+// isLoading = true;
+// album = 1998;
+
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// };
+
+// let postId: string | number;
